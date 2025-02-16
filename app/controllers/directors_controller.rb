7c72a0af7fea23a42f6ab2db510597eadff63811
ChangSonia <79300461+ChangSonia@users.noreply.github.com>
2025-02-16 15:13:08 +0000

    Director details

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,14 @@ class DirectorsController < ApplicationController
   def index
     render({:template => "director_templates/list"})
   end
+  
+  def show
+    the_id = params.fetch(:an_id)
+
+    matching_records = Director.where({:id => the_id}) # is a relationship, must convert into item to fetch item from the item
+
+    @the_director = matching_records.at(0)
+
+    render({:template => "director_templates/details"})
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
   get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:an_id", {:controller => "directors", :action => "show"})
   get("/directors/youngest", { :controller => "directors", :action => "youngest" })
   get("/directors/eldest", { :controller => "directors", :action => "eldest" })
   get("/movies", { :controller => "movies", :action => "index" })
   get("/actors", { :controller => "actors", :action => "index" })
+  get("/about.html",{:controller => "misc", :action => "about"})
 end
