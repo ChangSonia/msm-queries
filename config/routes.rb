45d54eafe62829b613a64a196e561896938c6eb1
ChangSonia <79300461+ChangSonia@users.noreply.github.com>
2025-02-16 16:15:23 +0000

    actors detail

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   get("/movies/:an_id", {:controller => "movies", :action => "show"})
 
   get("/actors", { :controller => "actors", :action => "index" })
+  get("/actors/:an_id", { :controller => "actors", :action => "show" })
+
 end
